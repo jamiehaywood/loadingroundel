b605ea25648f0c478f3c260b4e6e4a54dda8e120
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-31 15:29:35 +0100

    added base of site

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import LoadingRoundel from './LoadingRoundel'
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="center"><LoadingRoundel/></div>
+  );
+}
+
+export default App;
