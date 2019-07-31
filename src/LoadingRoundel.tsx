986895113881e83be52af3badd824a8c5f1fa5fa
Jamie Haywood <jamie.haywood@gmail.com>
2019-07-31 15:29:49 +0100

    added LoadingRoundel component

---
+++ src/LoadingRoundel.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './LoadingRoundel.css'
+
+const LoadingRoundel: React.FC = () => {
+    return (
+        <div>
+        <svg width="100px" height="80px" viewBox="0 0 500 400" version="1.1">
+            <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="TfL_roundel_(no_text)" transform="translate(0.000000, 34.000000)">
+                    <path
+                        d="M412.939753,165.99972 C412.95763,257.3511 339.09131,331.41554 247.96638,331.41554 C156.84146,331.41554 82.97514,257.3511 82.9930268,165.99972 C82.97514,74.64833 156.84146,0.58389 247.96638,0.58389 C339.09131,0.58389 412.95763,74.64833 412.939753,165.99972 L412.939753,165.99972 Z"
+                        id="pulsePath" stroke="#0019A8" stroke-width="69.1677704" className="pulse"></path>
+                    <path d="M412.939753,165.99972 C412.95763,257.3511 339.09131,331.41554 247.96638,331.41554 C156.84146,331.41554 82.97514,257.3511 82.9930268,165.99972 C82.97514,74.64833 156.84146,0.58389 247.96638,0.58389 C339.09131,0.58389 412.95763,74.64833 412.939753,165.99972 L412.939753,165.99972 Z"
+                        id="path3214" stroke="#0019A8" stroke-width="69.1677704"></path>
+
+                    <polygon id="path2259" fill="#0019A8" fill-rule="nonzero"
+                        points="0 126.01613 500 126.01613 500 204.03575 0 204.03575"></polygon>
+                </g>
+            </g>
+        </svg>
+        </div>
+    )
+}
+
+export default LoadingRoundel
